b094c42e50e623687d5e9a323181cf9b67dff591
raphael-caninde <raphael.caninde@gmail.com>
2022-02-21 01:25:43 -0300

    adicionando butões e css

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -4,6 +4,12 @@ import './FeaturedMovie.css';
 
 export default function FeaturedMovie() {
   const { featuredData } = useContext(MoviesContext);
+  let firstDate = new Date(featuredData.first_air_date);
+  let genres = [];
+
+  for (let i in featuredData.genres) {
+    genres.push(featuredData.genres[i].name)
+  }
 
   return (
     <section
@@ -21,10 +27,24 @@ export default function FeaturedMovie() {
           </div>
           <div className="featured--info">
             <div className="featured--points">{featuredData.vote_average} pontos</div>
-            <div className="featured--year">2099</div>
-            <div className="featured--year">
+            <div className="featured--year">{ firstDate.getFullYear() }</div>
+            <div className="featured--seasons">
               {featuredData.number_of_seasons} temporada{featuredData.number_of_seasons !== 1 ? 's' : '' }
             </div>
+          </div>
+          <div className="featured--description">
+            { featuredData.overview }
+          </div>
+          <div className="featured--buttons">
+            <a href={`/watch/${featuredData.id}`} className="featured--watchbutton">
+              Assitir
+            </a>
+            <a href={`/list/add/${featuredData.id}`} className="featured--mylistbutton">
+              + Minha Lista
+            </a>
+          </div>
+          <div className="featured--genres">
+            <strong>Gêneros:</strong> { genres.join(', ') }
           </div>
         </div>
       </div>
